e736847f9f6ffaf10245a5b4518e538c12d8a56b
Ashmit Bathla <ashbathla06@gmail.com>
2025-04-03 03:27:26 +0530

    fixed minor bugs

--- Scripts/Embedding-Model/train_embedding.py
+++ Scripts/Embedding-Model/train_embedding.py
@@ -1,27 +1,32 @@
-import yaml 
-# pytorch lightning 
-from pytorch_lightning import Trainer
-from pytorch_lightning.callbacks import DeviceStatsMonitor
-# TrackML Packages 
-from TrackML.Embedding.base import EmbeddingBase
-from TrackML.Embedding.dataset import EmbeddingDataset
 
-with open('hparams_embedding.yml' , 'r' ) as f : 
-    hparams = yaml.safe_load(f)
+if __name__ == "__main__":
+    import os
+    import yaml 
+    # pytorch lightning 
+    from pytorch_lightning import Trainer
+    from pytorch_lightning.callbacks import DeviceStatsMonitor
+    # TrackML Packages 
+    from TrackML.Embedding.base import EmbeddingBase
+    from TrackML.Embedding.dataset import EmbeddingDataset
 
-model = EmbeddingBase(hparams)
-ds = EmbeddingDataset(hparams)
 
-device_stats = DeviceStatsMonitor()
+    script_dir = os.path.dirname(os.path.abspath(__file__))  # Get script directory
+    file_path = os.path.join(script_dir, "hparams_embedding.yml")
 
-trainer = Trainer(
-    accelerator = "auto", 
-    devices = "auto",
-    enable_checkpointing=False, 
-    fast_dev_run = 3, 
-    callbacks=[device_stats]
-)
+    with open( file_path , 'r' ) as f : 
+        hparams = yaml.safe_load(f)
 
-print(f'Model Trainer Details : \n {trainer} ')
+    model = EmbeddingBase(hparams)
+    ds = EmbeddingDataset(hparams)
 
-trainer.fit(model , ds )
+    device_stats = DeviceStatsMonitor()
+
+    trainer = Trainer(
+        accelerator = "cpu", 
+        devices = "auto",
+        enable_checkpointing=False, 
+        fast_dev_run = 3, 
+        callbacks=[device_stats]
+    )
+
+    trainer.fit(model , ds )
